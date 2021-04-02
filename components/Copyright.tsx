5ba4b41e49ee7d963c639c9074c1f7692504e73b
pilgrim-1610 <54203519+pilgrim-1610@users.noreply.github.com>
2021-04-02 20:06:58 +0530

    Update Copyright.tsx

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -23,12 +23,20 @@ export default function Copyright() {
           <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
             {new Date().getFullYear()} WonderataX Solutions —
             <a
-              href="https://twitter.com/wonderatax"
+              href="Privacy policy.pdf"
               className="text-gray-600 ml-1"
               rel="noopener noreferrer"
               target="_blank"
             >
-              @wonderatax
+              Privacy policy
+            </a>
+                 <a
+              href="Return policy.pdf"
+              className="text-gray-600 ml-1"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Return Policy
             </a>
           </p>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
